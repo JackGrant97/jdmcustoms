593db2b30adf369a8915598f54904934225d78cf
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-05 14:26:32 +0100

    meme

--- index.php
+++ index.php
@@ -15,11 +15,6 @@
 		.mySlides {display:none}
 		.demo {cursor:pointer}
 	</style>
-  $ echo '{}' > composer.json
-  $ git add composer.json
-  $ git commit -m "add composer.json for PHP app detection"
-  $ heroku buildpacks:set heroku/php
-  $ heroku buildpacks:set https://github.com/heroku/heroku-buildpack-php
     <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
