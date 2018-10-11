ea84fa31accfd3d7bea5a00f1bb9983a2c0d575e
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-11 18:19:06 +0100

    login page created

--- js/js.js
+++ js/js.js
@@ -53,6 +53,14 @@
     });
 
   });
-}( jQuery ));
+  var app = angular.module('mainModule', []);
 
-       
+  app.controller('mainController', function($scope, $http){ //o scope liga o js e o template
+  $scope.nome = 'Valor Inicial';
+  //$http.get().success();
+  $scope.reset = function()
+  {
+  $scope.nome = '';
+  }
+  });
+}( jQuery ));
---
+++ login.php
@@ -0,0 +1,64 @@
+<html lang="en">
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="css/materialize.css">
+  <link type="text/css" rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+<style>
+  .mySlides {display:none}
+  .demo {cursor:pointer}
+</style>
+  <!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body ng-app="mainModule" ng-controller="mainController">
+<div id="login-page" class="row">
+    <div class="col s12 z-depth-6 card-panel">
+      <form class="login-form">
+        <div class="row">
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">mail_outline</i>
+            <input class="validate" id="email" type="email">
+            <label for="email" data-error="wrong" data-success="right">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock_outline</i>
+            <input id="password" type="password">
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12 m12 l12  login-text">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <a href="#" class="btn waves-effect waves-light col s12">Login</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s6 m6 l6">
+            <p class="margin medium-small"><a href="#">Register Now!</a></p>
+          </div>
+          <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+          </div>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</body>
+</html>
