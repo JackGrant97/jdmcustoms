a3f3368497b9e073017af4963f28e2ab3f62fe0c
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-11 18:34:28 +0100

    main

--- login.php
+++ login.php
@@ -45,49 +45,49 @@
           <script>$(".button-collapse").sideNav();</script>
   </header>
 <body background="assets/bg.jpeg" width="100%" height="100%">
-
-<div id="login-page" class="row">
-    <div class="col s12 z-depth-6 card-panel">
-      <form class="login-form">
-        <div class="row">
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">mail_outline</i>
-            <input class="validate" id="email" type="email">
-            <label for="email" data-error="wrong" data-success="right">Email</label>
+<main>
+  <div id="login-page" class="row">
+      <div class="col s12 z-depth-6 card-panel">
+        <form class="login-form">
+          <div class="row">
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">lock_outline</i>
-            <input id="password" type="password">
-            <label for="password">Password</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">mail_outline</i>
+              <input class="validate" id="email" type="email">
+              <label for="email" data-error="wrong" data-success="right">Email</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12 m12 l12  login-text">
-              <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Remember me</label>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">lock_outline</i>
+              <input id="password" type="password">
+              <label for="password">Password</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <a href="#" class="btn waves-effect waves-light col s12">Login</a>
+          <div class="row">
+            <div class="input-field col s12 m12 l12  login-text">
+                <input type="checkbox" id="remember-me" />
+                <label for="remember-me">Remember me</label>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s6 m6 l6">
-            <p class="margin medium-small"><a href="#">Register Now!</a></p>
+          <div class="row">
+            <div class="input-field col s12">
+              <a href="#" class="btn waves-effect waves-light col s12">Login</a>
+            </div>
           </div>
-          <div class="input-field col s6 m6 l6">
-              <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+          <div class="row">
+            <div class="input-field col s6 m6 l6">
+              <p class="margin medium-small"><a href="#">Register Now!</a></p>
+            </div>
+            <div class="input-field col s6 m6 l6">
+                <p class="margin right-align medium-small"><a href="#">Forgot password?</a></p>
+            </div>
           </div>
-        </div>
-
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
+  </main>
   <!--Footer-->
     <div class="footer">
       <footer class="page-footer grey darken-4">
