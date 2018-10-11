58534e0a67c5b0137aa9cc50db15bb6f51c8e962
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-11 18:08:42 +0100

    dbconnect.php created

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+//connect to mysql database
+$con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Error " .
+mysqli_error($con));
+?>
+
+//mysql://b35dd9c913bab7:2cd16625@eu-cdbr-west-02.cleardb.net/heroku_11b47e2296993b0?reconnect=true
