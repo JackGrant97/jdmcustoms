dccff49063e0ef3b00fa9b738bee974bf37c53c4
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-11 18:24:47 +0100

    login nav

--- login.php
+++ login.php
@@ -18,6 +18,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body ng-app="mainModule" ng-controller="mainController">
+  <nav class="z-depth-0">
+      <div class="nav-wrapper">
+      <a href="#!" class="brand-logo"></a>
+               <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+         <li><a href="inventory.php">Inventory</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="login.php">login</a></li>
+      </ul>
+    </div>
+  </nav>
 <div id="login-page" class="row">
     <div class="col s12 z-depth-6 card-panel">
       <form class="login-form">
