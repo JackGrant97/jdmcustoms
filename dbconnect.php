40fcebcd4b47b0a78e6a4256a1b4d53f9ed583e3
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-12 10:41:47 +0100

    dsa

--- dbconnect.php
+++ dbconnect.php
@@ -4,4 +4,4 @@ $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625
 mysqli_error($con));
 ?>
 
-//mysql://b35dd9c913bab7:2cd16625@eu-cdbr-west-02.cleardb.net/heroku_11b47e2296993b0?reconnect=true
+<!--mysql://b35dd9c913bab7:2cd16625@eu-cdbr-west-02.cleardb.net/heroku_11b47e2296993b0?reconnect=true!-->
