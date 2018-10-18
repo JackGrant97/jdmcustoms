fe30b1470cab6e92bbcec4f3198395965b0c6d5b
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-18 20:33:36 +0100

    Update login.php

--- login.php
+++ login.php
@@ -77,15 +77,8 @@
       </div>
     </div>
     <div class="status">
-      <?php
-        if (isset($_SESSION['userid']) {
-          echo '<p><b>You are Logged In!</b></p>';
-        }
-        else {
-          echo '<p><b>You are Logged out!</b></p>';
-        }
-       ?>
-
+       <p><b>You are Logged In!</b></p>
+       <p><b>You are Logged Out!</b></p>
     </div>
   </main>
 
