0490d9547444d265d044bb8c664d661629ef9e78
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 17:58:08 +0100

    Update includes/cart.inc.php

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -1,13 +1,1 @@
-<?php
 
-  require 'dbconnect.php';
-  $query = 'SELECT * FROM cart ORDER by itemID ASC';
-  $result = mysqli_query($con, $query);
-
-  if ($result) {
-    if (mysqli_num_rows($result) > 0) {
-      while ($product = mysqli_fetch_assoc($result)) {
-        print_r($product);
-      }
-    }
-  }
