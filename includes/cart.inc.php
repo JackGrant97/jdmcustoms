1ba8e4b52d63d6697d6a29f21ccab189bb334ab8
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:50:19 +0100

    df

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -1,1 +1,16 @@
+<?php
+  require 'dbconnect.php';
 
+  $query = "SELECT * FROM cart ORDER BY ItemID ASC"
+  $stmt = mysqli_stmt_init($con);
+  if (!mysqli_stmt_prepare($stmt, $query)) {
+    header("Location: ../inventory.php?error=sqlerror04");
+    exit();
+  }
+  $result = mysqli_stmt_get_result($stmt);
+  if ($result) {
+    while ($product = mysqli_fetch_assoc($result)) {
+      print_r($product);
+    }
+  }
+ ?>
--- inventory.php
+++ inventory.php
@@ -69,43 +69,21 @@
 
   <body background="assets/Backg.jpg" width="100%" height="100%" text="white">
 	 <main>
-     <?php
-       require 'dbconnect.php';
-
-       $itemID = $_POST['itemID'];
-       $itemName = $_POST['itemName'];
-       $itemPrice = $_POST['itemPrice'];
-       $itemImage = $_POST['itemImage'];
-
-       $sql = "SELECT * FROM cart ORDER BY ItemID ASC"
-       $stmt = mysqli_stmt_init($con);
-       if (!mysqli_stmt_prepare($stmt, $sql)) {
-         header("Location: ../inventory.php?error=sqlerror04");
-         exit();
-       }
-       $result = mysqli_stmt_get_result($stmt);
-       if ($result) {
-         while ($product = mysqli_fetch_assoc($result)) {
-           // code...
-         }
-       }
-      ?>
 	  <!--Cards-->
 	  <div class="container">
 	   <div class="content">
 		  <div class="row">
 		  <div class="col s10 m4">
-      <form method="post" action="includes/cart.inc.php?action=id<?php echo $product['itemID']; ?>">
 			<div class="card reveal-panel">
 			  <div class="card-image waves-effect waves-block waves-light">
 				<img class="activator" src="assets/cpu_thumb.png">
 			  </div>
 			<div class="card-content">
-				  <span class="card-title activator grey-text text-lighten-3"><?php echo $product['itemName']; ?><i class="material-icons right">more_vert</i></span>
+				  <span class="card-title activator grey-text text-lighten-3">CPU<i class="material-icons right">more_vert</i></span>
 				  <p class="grey-text text-lighten-3"><a href="cpu.html">Click Here to See Reviews</a></p>
 			</div>
 			  <div class="card-reveal">
-				<span class="card-title grey-text text-darken-4"><?php echo $product['itemName']; ?><i class="material-icons right">close</i></span>
+				<span class="card-title grey-text text-darken-4">CPU<i class="material-icons right">close</i></span>
 				<p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
 				</p>
 			  </div>
