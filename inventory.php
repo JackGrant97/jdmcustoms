6fe3cb45bcbaa70da3c36657ff3684a33673b50e
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:20:44 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -1,10 +1,5 @@
 <?php
   session_start();
-
-     require 'dbconnect.php';
-     $query = 'SELECT * FROM cart ORDER by itemID ASC';
-     $result = mysqli_query($con, $query);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
