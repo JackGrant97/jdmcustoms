081279564cde541e0c73a61a7b9f9db4a8532316
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:51:43 +0100

    Update includes/cart.inc.php

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -1,16 +1,16 @@
 <?php
   require 'dbconnect.php';
+  $query = 'SELECT * FROM cart ORDER by itemID ASC';
+  $result = mysqli_query($con, $query);
 
-  $query = "SELECT * FROM cart ORDER BY ItemID ASC"
-  $stmt = mysqli_stmt_init($con);
-  if (!mysqli_stmt_prepare($stmt, $query)) {
-    header("Location: ../inventory.php?error=sqlerror04");
-    exit();
-  }
-  $result = mysqli_stmt_get_result($stmt);
   if ($result) {
-    while ($product = mysqli_fetch_assoc($result)) {
-      print_r($product);
+    if (mysqli_num_rows($result) > 0) {
+      while ($product = mysqli_fetch_assoc($result)) {
+      print_r($result);
+        ?>
+
+        <?php
+      }
     }
   }
- ?>
+  ?>
