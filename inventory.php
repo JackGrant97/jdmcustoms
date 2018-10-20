703b5075202a36fa776244430f2764b9a8856dff
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:16:47 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -74,18 +74,8 @@
       require 'dbconnect.php';
       $query = 'SELECT * FROM cart ORDER by itemID ASC';
       $result = mysqli_query($con, $query);
+?>
 
-      if ($result):
-        if(mysqli_num_rows($result)>0):
-            while($product = mysqli_fetch_assoc($result)):
-            print_r($product);
-            ?>
-
-            <?php
-          endwhile;
-        endif;
-      endif;
-      ?>
 	 <main>
 	  <!--Cards-->
 	  <div class="container">
