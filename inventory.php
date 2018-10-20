9e300462e2afe329e138874db7b66a5917f403a3
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 17:16:23 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -83,10 +83,10 @@
             if (mysqli_num_rows($result) > 0) :
               while ($product = mysqli_fetch_assoc($result)) :
                 ?>
-                <div class="col s10 m4">
+              <div class="col s10 m4">
                <div class="card reveal-panel">
                  <div class="card-image waves-effect waves-block waves-light">
-                 <img class="activator" src="assets/cpu_thumb.png">
+                   <img class="activator" src="assets/cpu_thumb.png">
                  </div>
                <div class="card-content">
                    <span class="card-title activator grey-text text-lighten-3">CPU<i class="material-icons right">more_vert</i></span>
@@ -97,7 +97,7 @@
                  <p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
                  </p>
                  </div>
-               </div>
+                </div>
                </div>
                 <?php
               endwhile;
