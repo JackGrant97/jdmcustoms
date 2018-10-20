5a57b3a8090396ea7367eed63ac54b996245ea4d
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 20:08:09 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -74,20 +74,22 @@
 	   <div class="content">
 		  <div class="row">
 		  <div class="col s10 m4">
+      <form method="post" action="includes/cart.inc.php"
 			<div class="card reveal-panel">
 			  <div class="card-image waves-effect waves-block waves-light">
 				<img class="activator" src="assets/cpu_thumb.png">
 			  </div>
 			<div class="card-content">
-				  <span class="card-title activator grey-text text-lighten-3">CPU<i class="material-icons right">more_vert</i></span>
+				  <span class="card-title activator grey-text text-lighten-3"><?php echo $product['itemName'];?><i class="material-icons right">more_vert</i></span>
 				  <p class="grey-text text-lighten-3"><a href="cpu.html">Click Here to See Reviews</a></p>
 			</div>
 			  <div class="card-reveal">
-				<span class="card-title grey-text text-darken-4">CPU<i class="material-icons right">close</i></span>
+				<span class="card-title grey-text text-darken-4"><?php echo $product['itemName'];?><i class="material-icons right">close</i></span>
 				<p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
 				</p>
 			  </div>
 			</div>
+    </form>
 		  </div>
 		  <div class="col s10 m4">
 			<div class="card reveal-panel">
