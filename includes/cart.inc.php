9b4f3d8a31c1ca642a57175eb77e6a32a9542b98
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:59:47 +0100

    Update includes/cart.inc.php

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -1,9 +1,9 @@
 <?php
   require 'dbconnect.php';
-  $query = 'SELECT * FROM cart ORDER BY itemID ASC';
+  $query = 'SELECT * FROM cart ORDER by itemID ASC';
   $result = mysqli_query($con, $query);
 
-  if ($result) {
+  if ($result){
     if (mysqli_num_row($result) > 0) {
     while ($product = mysqli_fetch_assoc($result)) {
         print_r($product);
