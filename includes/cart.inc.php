26b2bcaf2031c60072e7df78e79e94ff7919232b
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-20 19:52:25 +0100

    Update includes/cart.inc.php

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -4,7 +4,7 @@
   $result = mysqli_query($con, $query);
 
   if ($result) {
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_stmt_get_result($result) > 0) {
       while ($product = mysqli_fetch_assoc($result)) {
       print_r($result);
         ?>
