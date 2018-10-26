9977d0d5f8857a682cea5d251bc7f0b99d3fa002
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-26 15:37:43 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -81,7 +81,29 @@
            while($product = mysqli_fetch_assoc($result)):
         ?>
                 <div class="row">
-                    <div class="col s6 col m5">
+                    <div class="col m6">
+                      <form method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
+                        <div class="products">
+     			              <div class="card reveal-panel">
+     			                <div class="card-image waves-effect waves-block waves-light">
+     				                <img class="activator" src="<?php echo $product['image1']?>">
+     			               </div>
+                     			<div class="card-content">
+                     				  <span class="card-title activator grey-text text-lighten-3"><?php echo $product['itemName']?> Price: £<?php echo $product['itemPrice']?><i class="material-icons right">more_vert</i></span>
+                     				  <p class="grey-text text-lighten-3"><a href="cpu.html">Click Here to See Reviews</a></p>
+                     			</div>
+                   			  <div class="card-reveal">
+                     				<span class="card-title grey-text text-darken-4"><?php echo $product['itemName']?> Price: £<?php echo $product['itemPrice']?><i class="material-icons right">close</i></span>
+                     				<p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
+                     				</p>
+                   			  </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="row">
+                    <div class="col m6">
                       <form method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
                         <div class="products">
      			              <div class="card reveal-panel">
