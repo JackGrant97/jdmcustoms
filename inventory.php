22af3d8569680acd08fb87401c100766656a4743
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-10-26 15:52:42 +0100

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -81,9 +81,8 @@
            while($product = mysqli_fetch_assoc($result)):
         ?>
                 <div class="row">
-
+                    <div class="col s3 col m3 center-align">
                       <form method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
-                        <div class="col s3 col m3 center-align">
                         <div class="products">
      			              <div class="card reveal-panel">
      			                <div class="card-image waves-effect waves-block waves-light">
@@ -100,9 +99,8 @@
                    			  </div>
                         </div>
                       </div>
-
+                    </form>
                   </div>
-                  </form>
                 </div>
            <?php
          endwhile;
