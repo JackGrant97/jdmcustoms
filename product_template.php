b66f4d9d1afbbf26e208b17dc6b1a9e9a1fc67f9
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 15:13:27 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -69,6 +69,16 @@
     </header>
   <body background="assets/Backg.jpg" width="100%" height="100%" text="white">
       <main>
+        <?php
+          $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
+          mysqli_error($con));
+          $query = 'SELECT * FROM products ORDER by itemID ASC';
+          $result = mysqli_query($con, $query);
+
+          if ($result):
+            if(mysqli_num_rows($result) > 0):
+              while($product = mysqli_fetch_assoc($result)):
+           ?>
       <div class="slide">
       <div class="container">
             <div class="carousel carousel-slider center" data-indicators="true">
