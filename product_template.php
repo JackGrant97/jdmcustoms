5a0c6519b1298a048bd581bbe09a91b78347edf8
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 15:05:17 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -68,6 +68,8 @@
             <script>$(".button-collapse").sideNav();</script>
     </header>
     <?php
+    $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
+    mysqli_error($con));
     if (isset($_POST['itemID']))
     {
        $itemID = $_POST['itemID'];
