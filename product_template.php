aaab65dea52b4dc52bb82d112f68f2692e85fdca
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 18:35:26 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -74,12 +74,13 @@
           $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
           mysqli_error($con));
           $id = intval($_GET['itemID']);
-          $query = mysqli_query("SELECT * FROM products WHERE itemID = ".$id);
+          $query = mysqli_query('SELECT * FROM products WHERE itemID = $id');
           $result = mysqli_query($con, $query);
 
           if ($result):
             if(mysqli_num_rows($result) > 0):
               while($product = mysqli_fetch_assoc($result)):
+                echo $product['itemMake'];
            ?>
           <div class="slide">
             <div class="container">
