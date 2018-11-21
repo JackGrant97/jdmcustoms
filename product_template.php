20eab60c96731041c972e0d6fd6e4dbcbe82f2f8
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 15:56:38 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -69,17 +69,18 @@
     </header>
   <body background="assets/Backg.jpg" width="100%" height="100%" text="white">
       <main>
-      <div class="slide">
-      <div class="container">
-            <div class="carousel carousel-slider center" data-indicators="true">
-               <a class="carousel-item" href="#one!"><img src="http://via.placeholder.com/900x500"></a>
-                  <a class="carousel-item" href="#two!"><img src="http://via.placeholder.com/900x500"></a>
-                  <a class="carousel-item" href="#three!"><img src="http://via.placeholder.com/900x500"></a>
-                  <a class="carousel-item" href="#four!"><img src="http://via.placeholder.com/900x500"></a>
+        <div class="maincontent">
+          <div class="slide">
+            <div class="container">
+                <div class="carousel carousel-slider center" data-indicators="true">
+                   <a class="carousel-item" href="#one!"><img src="http://via.placeholder.com/900x500"></a>
+                      <a class="carousel-item" href="#two!"><img src="http://via.placeholder.com/900x500"></a>
+                      <a class="carousel-item" href="#three!"><img src="http://via.placeholder.com/900x500"></a>
+                      <a class="carousel-item" href="#four!"><img src="http://via.placeholder.com/900x500"></a>
+                </div>
             </div>
-        </div>
-     </div>
-    </div>
+         </div>
+
          <script>
               $('.carousel.carousel-slider').carousel({fullWidth: true});
         </script>
