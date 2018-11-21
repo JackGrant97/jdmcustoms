c3ae2dba30ffcf45d4fce77973d6f612268afa13
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 16:21:41 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -77,6 +77,10 @@
             $query = 'SELECT * FROM products ORDER by itemID ASC';
             $result = mysqli_query($con, $query);
 
+            if ($result):
+              if(mysqli_num_rows($result) > 0):
+                while($product = mysqli_fetch_assoc($result)):
+          
             $_GET['itemID'];
 
              ?>
