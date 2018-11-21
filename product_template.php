ce47e48fdd91b91817f5439587baf3c8499b74f0
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 15:33:36 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -72,13 +72,10 @@
         <?php
           $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
           mysqli_error($con));
-          $query = 'SELECT * FROM products ORDER by itemID ASC';
-          $result = mysqli_query($con, $query);
-
-          if ($result):
-            if(mysqli_num_rows($result) > 0):
-              while($product = mysqli_fetch_assoc($result)):
-           ?>
+          $productid = $_REQUEST['itemID']''
+          $result = mysqli_query( "SELECT * FROM products WHERE itemID ='{$productid}");
+          $product = mysqli_fetch_assoc($result)):
+        ?>
       <div class="slide">
       <div class="container">
             <div class="carousel carousel-slider center" data-indicators="true">
