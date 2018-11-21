58c4548d54f736fb1efa6ce1a7828de841b454c1
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 17:33:35 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -74,7 +74,7 @@
           $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
           mysqli_error($con));
           $product_id = $_GET['itemID'];
-          $query = mysqli_query('SELECT * FROM products WHERE itemID = $product_id');
+          $query = mysqli_query('SELECT * FROM products WHERE itemID ='.$product_id);
           $result = mysqli_query($con, $query);
 
           if ($result):
