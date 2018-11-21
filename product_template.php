4d501315e0e0c69d66c639e1ab69bcc2c94d5634
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-21 19:38:25 +0000

    Update product_template.php

--- product_template.php
+++ product_template.php
@@ -84,9 +84,9 @@
             <div class="container">
                 <div class="carousel carousel-slider center" data-indicators="true">
                    <a class="carousel-item" href="#one!"><img src="<?php echo $product['image1']?>"></a>
-                      <a class="carousel-item" href="#two!"><img src="http://via.placeholder.com/900x500"></a>
-                      <a class="carousel-item" href="#three!"><img src="http://via.placeholder.com/900x500"></a>
-                      <a class="carousel-item" href="#four!"><img src="http://via.placeholder.com/900x500"></a>
+                      <a class="carousel-item" href="#two!"><img src="<?php echo $product['image2']?>"></a>
+                      <a class="carousel-item" href="#three!"><img src="<?php echo $product['image2']?>"></a>
+                      <a class="carousel-item" href="#four!"><img src="<?php echo $product['image3']?>"></a>
                 </div>
             </div>
          </div>
