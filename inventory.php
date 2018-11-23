3bead0a0d145c502cf2427e0a3e872b3599474e0
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 20:31:48 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -73,7 +73,7 @@
      <div class="container">
        <form class="searchbar">
          <div class="input-field">
-           <input id="search" type="search" required>
+           <input class="searchinput" id="search" type="search" required>
            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
            <i class="material-icons">close</i>
          </div>
