ffeed3ab80fe592a90b2a34bdc7c796b1b649bad
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 20:20:36 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -69,14 +69,15 @@
     </header>
   <body background="assets/Backg.jpg" width="100%" height="100%" text="white">
 	 <main>
-          <form class="sbar">
-            <div class="input-field">
-              <input id="search" type="search" required>
-              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-              <i class="material-icons">close</i>
-            </div>
-          </form>
+
      <div class="container">
+       <form class="sbar">
+         <div class="input-field">
+           <input id="search" type="search" required>
+           <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+           <i class="material-icons">close</i>
+         </div>
+       </form>
      <!--<div class="row">
        <div class="sidemenu">
         <ul id="slide-out" class="side-nav fixed">
