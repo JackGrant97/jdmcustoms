7f12a0c6ee2c5fbe585a0c966216a4005f475d72
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 19:48:51 +0000

    asd

--- inventory.php
+++ inventory.php
@@ -74,7 +74,7 @@
        <div class="sidemenu">
         <ul id="slide-out" class="side-nav fixed">
           <div class="searchbar">
-            <form>
+            <form class="sbar">
               <div class="input-field">
                 <input id="search" type="search" required>
                 <label class="label-icon" for="search"><i class="material-icons">search</i></label>
