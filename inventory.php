c5aea8bbeb5e94c4b7fba3d31d46d0cb0ebcf38c
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 19:00:56 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -72,6 +72,13 @@
      <div class="container">
      <div class="row">
        <div class="sidemenu">
+         <form>
+           <div class="input-field">
+             <input id="search" type="search" required>
+             <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+             <i class="material-icons">close</i>
+           </div>
+         </form>
         <!-- <ul id="slide-out" class="side-nav fixed">
           <div class="searchbar">
             <form>
