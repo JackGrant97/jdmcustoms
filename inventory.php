a55cb9a46831e4bff7929ebe25baedff32937419
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 20:22:58 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -103,7 +103,7 @@
              if(mysqli_num_rows($result) > 0):
                while($product = mysqli_fetch_assoc($result)):
             ?>
-                <div class="col s12 m12 l3">
+                <div class="col s12 m3 l3">
                   <form class="car" method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
                     <div class="products">
      			            <div class="card reveal-panel">
