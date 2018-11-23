7d6a2c00d8c749a0489034da02d2e673a66858b1
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 16:31:06 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -72,7 +72,7 @@
 	 <main>
     <div class="cars">
      <div class="row">
-       <div class="col s12 m4 l3">
+       <div class="col s12 m3 l3">
          <div class="sidemenu">
            <p class="waves-effect waves-light btn"><a href="product_template.php?itemID=<?php echo $product['itemID'];?>">More Details</a></p>
          </div>
@@ -88,7 +88,7 @@
              if(mysqli_num_rows($result) > 0):
                while($product = mysqli_fetch_assoc($result)):
             ?>
-            <div class="col s12 m8 l9"> 
+            <div class="col s12 m8 l9">
                 <div class="col s4 ">
                   <form method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
                     <div class="products">
