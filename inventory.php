decd3c31a0857d161da2d16cda95230c8ebca566
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-23 16:10:01 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -78,7 +78,7 @@
 
          </div>
        </div>
-
+       <div class="container">
          <?php
            $con = mysqli_connect("eu-cdbr-west-02.cleardb.net", "b35dd9c913bab7", "2cd16625", "heroku_11b47e2296993b0") or die("Connection Failed" .
            mysqli_error($con));
@@ -114,7 +114,7 @@
        endif;
      endif;
      ?>
-
+   </div>
  </div>
 </div>
 	  <!--Cards
