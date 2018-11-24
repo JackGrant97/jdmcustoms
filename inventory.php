e1b08b37a146eb35ff62cec19a8636c3d4b9a7cb
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-11-24 16:46:03 +0000

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -90,7 +90,7 @@
                while($product = mysqli_fetch_assoc($result)):
             ?>
             <?php
-            if (isset($_POST['Search'])) {
+            if (isset($_POST['search'])) {
                 $search = mysqli_real_escape_string($con, $_POST['Search']);
                 $sql = "SELECT * FROM products WHERE itemMake LIKE '%$search%' OR itemModel LIKE '%$search%' OR itemYear LIKE '%$search%' trans LIKE '%$search%' fuelType LIKE '%$search%'";
                 $result = mysqli_query($con, $sql);
@@ -98,7 +98,7 @@
 
                 if ($queryResult > 0 ) {
                   while ($row = mysqli_fetch_assoc($result)) {
-                    
+
                   }
                 } else {
                   echo "There Are No Results Matching Your Search";
