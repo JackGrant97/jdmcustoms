4e4c48d4a18729330caa3ac8a1dcc7887fa71f10
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-12-06 20:25:03 +0000

    Update search.php

--- search.php
+++ search.php
@@ -94,35 +94,33 @@
 
              if ($queryResult > 0) {
                while ($row = mysqli_fetch_assoc($result)) {
-                 echo "  <div class="col s12 m12 l4">
-                     <form class="car" method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
-                       <div class="products">
-        			            <div class="card reveal-panel">
-        			             <div class="card-image waves-effect waves-block waves-light">
-        				            <img class="activator" src="<?php echo $product['image1']?>">
-        			             </div>
-                        	 <div class="card-content">
-                        		 <span class="card-title activator grey-text text-lighten-3"><?php echo $product['itemMake']?>: <?php echo $product['itemModel']?><br>Price: £<?php echo $product['itemPrice']?><i class="material-icons right">more_vert</i></span>
-                        		 <p class="waves-effect waves-light btn"><a href="product_template.php?itemID=<?php echo $product['itemID'];?>">More Details</a></p>
-                        	 </div>
-                      		 <div class="card-reveal">
-                        	   <span class="card-title grey-text text-darken-4"><?php echo $product['itemMake']?>: <?php echo $product['itemModel']?><br>Price: £<?php echo $product['itemPrice']?><i class="material-icons right">close</i></span>
-                        		 <p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
-                        		</p>
-                      		</div>
-                       </div>
-                     </div>
-                   </form>
-                 </div>";
+                 echo "";
                }
              } else {
                echo "There are no results matching your search!";
              }
            }
             ?>
-
-               <?php
-
+                <div class="col s12 m12 l4">
+                  <form class="car" method="post" action="inventory.php?actionid<?php echo $product['itemID'];?>">
+                    <div class="products">
+     			            <div class="card reveal-panel">
+     			             <div class="card-image waves-effect waves-block waves-light">
+     				            <img class="activator" src="<?php echo $product['image1']?>">
+     			             </div>
+                     	 <div class="card-content">
+                     		 <span class="card-title activator grey-text text-lighten-3"><?php echo $product['itemMake']?>: <?php echo $product['itemModel']?><br>Price: £<?php echo $product['itemPrice']?><i class="material-icons right">more_vert</i></span>
+                     		 <p class="waves-effect waves-light btn"><a href="product_template.php?itemID=<?php echo $product['itemID'];?>">More Details</a></p>
+                     	 </div>
+                   		 <div class="card-reveal">
+                     	   <span class="card-title grey-text text-darken-4"><?php echo $product['itemMake']?>: <?php echo $product['itemModel']?><br>Price: £<?php echo $product['itemPrice']?><i class="material-icons right">close</i></span>
+                     		 <p>A CPU also known as a Central Processing Unit, or processor is referred to as the brain of a computer system. A CPU completes most calculations. Without a CPU a computer system will be unable to operate as a CPU allocates tasks to each component of the system, this includes hard disks, RAM, GPU's etc.
+                     		</p>
+                   		</div>
+                    </div>
+                  </div>
+                </form>
+              </div>
       </div>
     </div>
 	  </main>
