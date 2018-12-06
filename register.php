154a071a6ad5a1c66a7f54bbd9c51c0fda4edd48
Jack Grant <g1492622@student.southstaffs.ac.uk>
2018-12-06 23:16:34 +0000

    Update register.php

--- register.php
+++ register.php
@@ -73,46 +73,46 @@
   <main>
     <div class="register">
       <div class="row">
-        <form class="col s12" action="includes/signup.inc.php" method="post">
+        <form class="col s12 m12 l12" action="includes/signup.inc.php" method="post">
           <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s6 m6 l6">
               <input id="first_name" name="FirstName" type="text" class="validate">
               <label for="first_name">First Name</label>
             </div>
-            <div class="input-field col s6">
+            <div class="input-field col s6 m6 l6">
               <input id="last_name" name="LastName" type="text" class="validate">
               <label for="last_name">Last Name</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s6">
+            <div class="input-field col s6 m6 l6">
               <input id="postcode" name="postcode" type="text" class="validate">
               <label for="postcode">Postcode</label>
             </div>
-            <div class="input-field col s6">
+            <div class="input-field col s6 m6 l6">
               <input id="City" name="City" type="text" class="validate">
               <label for="City">City</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s12">
+            <div class="input-field col s12 m12 l12">
               <input id="address" name="address" type="text" class="validate">
               <label for="address">Address</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s12">
+            <div class="input-field col s12 m12 l12">
               <input id="email" name="email" type="email" class="validate">
               <label for="email">E-mail</label>
             </div>
           </div>
           <div class="row">
-            <div class="input-field col s12">
+            <div class="input-field col s12 m12 l12">
               <input id="password" name="password" type="password" class="validate">
               <label for="password">Password</label>
             </div>
           </div><div class="row">
-            <div class="input-field col s12">
+            <div class="input-field col s12 m12 l12">
               <input id="password" name="password-repeat" type="password" class="validate">
               <label for="password">Confirm Password</label>
             </div>
