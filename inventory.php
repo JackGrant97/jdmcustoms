44dc7ed6fe6d4767b0c6bc6e99e36bbb575b5e14
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-01-21 18:48:29 +0000

    s

--- inventory.php
+++ inventory.php
@@ -67,7 +67,7 @@
         </ul>
             <script>$(".button-collapse").sideNav();</script>
     </header>
-  <body background="assets/CarBG5.jpg" width="100%" height="100%" text="white">
+  <body background="assets/CarBG6.jpg" width="100%" height="100%" text="white">
 	 <main>
      <div class="container">
        <form action="search.php" method="POST">
