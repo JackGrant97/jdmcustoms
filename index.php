8e3329cd1767d457afb3805c59da0df1e31d33dd
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-01-31 15:22:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,10 @@
         </ul>
             <script>$(".button-collapse").sideNav();</script>
     </header>
-  <body background="assets/videos/jdmcar.mp4" width="100%" height="100%">
+  <body  width="100%" height="100%">
+    <video controls loop>
+      <source src="assets/videos/jdmcar.mp4">
+    </video>
    	<main>
         <div class="container">
             <div class="logo">
