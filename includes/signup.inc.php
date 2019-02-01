baccf6d896c0b9260d3a8b4f947e2edbcd8e3669
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-01 16:12:34 +0000

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -41,6 +41,10 @@ if (isset($_POST['signup-submit'])) {
     header("Location: ../register.php?error=invalidCity&FirstName=".$firstname."&LastName=".$lastname."&postcode=".$postcode."&address=".$address);
     exit();
   }
+  else if (!preg_match("/^[a-zA-Z]*$/",$dateofbirth)) {
+    header("Location: ../register.php?error=invalidDateofBirth&FirstName=".$firstname."&LastName=".$lastname."&postcode=".$postcode."&address=".$address);
+    exit();
+  }
   else if ($password !== $passwordRepeat) {
     header("Location: ../register.php?error=passwordcheck&FirstName=".$firstname."&LastName=".$lastname."&postcode=".$postcode."&address=".$address);
     exit();
