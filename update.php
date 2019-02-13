863cd65f54525dd4bc0a4d5583b3902c8c6c0b5f
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-13 21:43:23 +0000

    Update update.php

--- update.php
+++ update.php
@@ -127,7 +127,7 @@
             </div>
           </div>
           <div class="submit">
-              <button class="waves-effect waves-light btn" type="submit" name="signup-submit">SIGNUP
+              <button class="waves-effect waves-light btn" type="submit" name="signup-submit">Confirm Changes
                 <i class="material-icons right">add</i>
               </button>
           </div>
