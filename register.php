aacf3840c2ea2151afe2b3dc46b67b9685b733e2
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-15 18:16:37 +0000

    Update register.php

--- register.php
+++ register.php
@@ -99,7 +99,7 @@
           </div>
           <div class="row">
             <div class="input-field col s12 m12 l12">
-              <input id="telephone" name="telephone" type="text" pattern="[0-9]" title="Error! Please Use Numbers" class="validate">
+              <input id="telephone" name="telephone" type="text" class="validate">
               <label for="telephone">Telephone Number</label>
             </div>
           </div>
