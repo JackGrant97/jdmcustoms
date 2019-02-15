2626ba6719a9e6852fb9d4bd548bd68c5789c827
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-15 17:41:00 +0000

    Update register.php

--- register.php
+++ register.php
@@ -79,11 +79,11 @@
         <form class="col s12 m12 l12" action="includes/signup.inc.php" method="post">
           <div class="row">
             <div class="input-field col s6 m6 l6">
-              <input id="first_name" name="FirstName" type="text" pattern="[A-Za-z]" class="validate">
+              <input id="first_name" name="FirstName" type="text" pattern="[A-Za-z]" title="Error! Please Use Letters" class="validate">
               <label for="first_name">First Name</label>
             </div>
             <div class="input-field col s6 m6 l6">
-              <input id="last_name" name="LastName" type="text" pattern="[A-Za-z]" class="validate">
+              <input id="last_name" name="LastName" type="text" pattern="[A-Za-z]" title="Error! Please Use Letters" class="validate">
               <label for="last_name">Last Name</label>
             </div>
           </div>
@@ -93,13 +93,13 @@
               <label for="postcode">Postcode</label>
             </div>
             <div class="input-field col s6 m6 l6">
-              <input id="City" name="City" type="text" pattern="[A-Za-z]" class="validate">
+              <input id="City" name="City" type="text" pattern="[A-Za-z]" title="Error! Please Use Letters" class="validate">
               <label for="City">City</label>
             </div>
           </div>
           <div class="row">
             <div class="input-field col s12 m12 l12">
-              <input id="telephone" name="telephone" type="number" pattern="[0-9]" class="validate">
+              <input id="telephone" name="telephone" type="number" pattern="[0-9]" title="Error! Please Use Numbers" class="validate">
               <label for="telephone">Telephone Number</label>
             </div>
           </div>
