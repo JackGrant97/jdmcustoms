999e6d67189f663e54eabdea2aae360ec65125b0
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-15 17:19:21 +0000

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -76,7 +76,7 @@ if (isset($_POST['signup-submit'])) {
           //Uses BCrypt to hash users password
           $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
 
-          mysqli_stmt_bind_param($stmt, "ssssssssi", $firstname, $lastname, $email, $hashedpassword, $postcode, $address, $city, $Dob, $num);
+          mysqli_stmt_bind_param($stmt, "ssssssssn", $firstname, $lastname, $email, $hashedpassword, $postcode, $address, $city, $Dob, $num);
           mysqli_stmt_execute($stmt);
           header("Location: ../register.php?signup=success");
           exit();
