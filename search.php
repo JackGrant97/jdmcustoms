ded1a26bb18d4cdbb3e966a98c93866f92f5b0d8
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-21 18:40:24 +0000

    Update search.php

--- search.php
+++ search.php
@@ -88,11 +88,14 @@
                      $query = "SELECT * FROM products WHERE itemMake LIKE '%$search%' OR itemModel LIKE '%$search%' OR itemYear LIKE '%$search%' OR trans LIKE '%$search%' OR fuelType LIKE '%$search%' OR itemColour LIKE '%$search%'";
                      $result = mysqli_query($con, $query);
 
-                     $save = mysqli_query($con, $sql);
-                     $sql = "INSERT INTO search (keyword, UserID) VALUES (?, $id )";
-                     mysqli_stmt_bind_param($save, "ss", $save, $id);
-                     mysqli_stmt_execute($save);
-
+                     $sql = "INSERT INTO users (keyword) VALUES (?)";
+                     $stmt = mysqli_stmt_init($con);
+                     if (!mysqli_stmt_prepare($stmt, $sql)) {
+                       header("Location: ../register.php?error=sqlerror02");
+                       exit();
+                     }
+                     mysqli_stmt_bind_param($stmt, "s", $keyword);
+                     mysqli_stmt_execute($stmt);
                      if ($result):
                        if(mysqli_num_rows($result) > 0):
                          while($product = mysqli_fetch_assoc($result)):
