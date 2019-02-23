7dd71590fbd76c2d3b0d16a2d7c7777b7b506a12
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-23 17:54:53 +0000

    Update search.php

--- search.php
+++ search.php
@@ -86,7 +86,7 @@
 
 
                    if (isset($_POST['submit-fsearch'])) {
-                     $sql = "INSERT INTO fsearch (keyword, UserID) VALUES ($_SESSION['search'], $id)";
+                     $sql = "INSERT INTO fsearch (keyword, UserID) VALUES (?, ?)";
                      $stmt = mysqli_stmt_init($con);
                      if (!mysqli_stmt_prepare($stmt, $sql)) {
                        header("Location: ../search.php?error=sqlerror02");
@@ -94,6 +94,7 @@
                      }
 
                    }
+                   mysqli_stmt_bind_param($stmt, "ss",  $_SESSION['search'], $id);
                    mysqli_stmt_execute($stmt);
 
                    if (isset($_POST['submit-search'])):
