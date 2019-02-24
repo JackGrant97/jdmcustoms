dc414a8ce89c3f01f3867f34475f5e4257f64e29
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-24 16:07:40 +0000

    Update myprofile.php

--- myprofile.php
+++ myprofile.php
@@ -79,7 +79,7 @@
       $query = "SELECT FirstName, LastName, email, postcode, address, Dob, telephone FROM users WHERE UserID = $id";
       $result = mysqli_query($con, $query);
 
-      $sql = "SELECT * FROM search WHERE UserID = $id ORDER by keyword ASC";
+      $sql = "SELECT keyword FROM search WHERE UserID = $id ORDER by keyword ASC";
         $sresult = mysqli_query($con, $sql);
           if ($sresult):
             if (mysqli_num_rows($sresult) > 0):
