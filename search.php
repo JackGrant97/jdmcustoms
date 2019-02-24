941315c14d432e28aac8ce997b3e1d7b3ff47c27
Jack Grant <g1492622@student.southstaffs.ac.uk>
2019-02-24 17:01:51 +0000

    Update search.php

--- search.php
+++ search.php
@@ -84,18 +84,6 @@
                    $id = $_SESSION['userid'];
                    $keyword = $_POST['search'];
 
-                   if (isset($_POST['submit-fsearch'])) {
-                     $sql = "INSERT INTO fsearch (keyword, UserID) VALUES (?, ?)";
-                     $stmt = mysqli_stmt_init($con);
-                     if (!mysqli_stmt_prepare($stmt, $sql)) {
-                       header("Location: ../search.php?error=sqlerror02");
-                       exit();
-                     }
-                     mysqli_stmt_bind_param($stmt, "ss", $keyword, $id);
-                     mysqli_stmt_execute($stmt);
-                   }
-
-
                    if (isset($_POST['submit-search'])):
                      $search = mysqli_real_escape_string($con, $_POST['search']);
                      $query = "SELECT * FROM products WHERE itemMake LIKE '%$search%' OR itemModel LIKE '%$search%' OR itemYear LIKE '%$search%' OR trans LIKE '%$search%' OR fuelType LIKE '%$search%' OR itemColour LIKE '%$search%' OR engineSize LIKE '%$search%'";
